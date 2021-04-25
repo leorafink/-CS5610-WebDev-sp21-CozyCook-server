6ea616ac88780f09bfb77bcc4345306da0b09e9f
leorafink1 <fink.le@husky.neu.edu>
2021-04-25 17:32:08 -0400

    started attempting users who liked recipe

--- src/main/java/com/example/cozycook/repositories/RecipeRepository.java
+++ src/main/java/com/example/cozycook/repositories/RecipeRepository.java
@@ -2,6 +2,7 @@ package com.example.cozycook.repositories;
 
 import java.util.List;
 
+import com.example.cozycook.models.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -16,10 +17,10 @@ public interface RecipeRepository extends CrudRepository<Recipe, Long> {
   @Query(value="SELECT * FROM recipes WHERE user_id=:uid ORDER BY id DESC LIMIT 1", nativeQuery = true)
   public Recipe findMostRecentRecipe(@Param("uid") Long userId);
 
-  @Query(value="SELECT user_id FROM recipes WHERE id=:recipeId", nativeQuery = true)
-  public List<Long> findUsersWhoLikeThisRecipe(@Param("recipeId") Long recipeId);
+  @Query(value="SELECT users.* FROM users, recipes WHERE recipes.user_id=users.id AND recipes.id=:recipeId", nativeQuery = true)
+  public List<User> findUsersWhoLikeThisRecipe(@Param("recipeId") Long recipeId);
 
-  @Query(value="SELECT users.username FROM users, recipes WHERE recipes.user_id=users.id AND recipes.id=:recipeId")
+ // @Query(value="SELECT users.username FROM users, recipes WHERE recipes.user_id=users.id AND recipes.id=:recipeId")
 
-  @Query(value="SELECT username FROM users LEFT JOIN recipes ON users.id == recipes.user_id WHERE user_id=:uid")
+ // @Query(value="SELECT username FROM users LEFT JOIN recipes ON users.id == recipes.user_id WHERE user_id=:uid")
 }

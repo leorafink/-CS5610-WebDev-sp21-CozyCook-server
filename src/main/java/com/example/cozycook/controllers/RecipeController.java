4825dc2700e5853efb45eabee3f4c7d35039b4fb
AriellesMacBook <arielleslatus@login.khoury.neu.edu>
2021-04-26 13:21:27 -0400

    *

--- src/main/java/com/example/cozycook/controllers/RecipeController.java
+++ src/main/java/com/example/cozycook/controllers/RecipeController.java
@@ -23,7 +23,8 @@ import javax.servlet.http.HttpSession;
 
 @RestController
 // Local: @CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
-@CrossOrigin(origins = "http://cs5610-charlotteswebdev-client.herokuapp.com", allowCredentials = "true")
+// http://cs5610-charlotteswebdev-client.herokuapp.com
+@CrossOrigin(origins = "*", allowCredentials = "true")
 public class RecipeController {
   @Autowired
   RecipeService service;
